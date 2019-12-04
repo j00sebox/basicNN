997029156b38f04b9736e12c5d8748df131a0e7e
unknown <josharms00@gmail.com>
2019-12-03 21:12:36 -0500

    Initialized weights and biases and added functionality to compute activation values for the hidden layer.

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from nn import NeuralNetwork
+import numpy as np
 
 
 def main():
     x = NeuralNetwork(2, 3, 2)
-    print(x.i)
-    print(x.h)
-    print(x.o)
-    print(x.sig)
+    # print(x.i)
+    # print(x.h)
+    # print(x.o)
+
+    # # weight matrixes
+    # print(x.weights_1)
+    # print(x.weights_2)
+
+    # # bias matrixes
+    # print(x.bias_1)
+    # print(x.bias_2)
+
+    i = np.random.randint(1.0, size=(x.i, 1))
+    print(x.guess(i))
+
+
 
 if __name__ == "__main__":
     main()
--- nn.py
+++ nn.py
@@ -1,13 +1,29 @@
 import math
+import numpy as np
 
+# sigmod function for neuron activation values
 def sigmoid(x):
-    result = 1 / (1 + math.exp(-x))
+    result = 1 / (1 + np.exp(-x))
     return result
 
+# Class to instantiate network structure
+class NeuralNetwork(object):
+    hActivation = []
 
-class NeuralNetwork:
     def __init__(self, inputNodes, hiddenNodes, outputNodes):
         self.i = inputNodes
         self.h = hiddenNodes
         self.o = outputNodes
-        self.sig = sigmoid(200)
+
+        # intialize random weights for both layers
+        self.weights_1 = np.random.randint(1.0, size=(hiddenNodes, inputNodes))
+        self.weights_2 = np.random.randint(1.0, size=(hiddenNodes, outputNodes))
+        
+        # initialize random biases 
+        self.bias_1 = np.random.randint(10, size=(hiddenNodes, 1))
+        self.bias_2 = np.random.randint(10, size=(outputNodes, 1))
+
+    def guess(self, data):
+        self.hActivation = sigmoid(self.weights_1.dot(data) + self.bias_1)
+        return self.hActivation
+        
