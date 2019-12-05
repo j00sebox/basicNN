Bug Fix: moved the calculation of hidden error before the weight matrix is adjusted.

nn.py
@@ -9,10 +9,9 @@ def sigmoid(x):
 
 # Class to instantiate network structure
 class NeuralNetwork(object):
-    hActivation = []
 
     # default learning rate for the network
-    lr = 0.5
+    lr = 0.1
 
     def __init__(self, inputNodes, hiddenNodes, outputNodes, outputLabels):
         # initialize network structure
@@ -24,12 +23,12 @@ class NeuralNetwork(object):
         self.labels = outputLabels
 
         # intialize random weights for both layers
-        self.weights_1 = np.random.uniform(0.0, 1.01, size=(hiddenNodes, inputNodes))
-        self.weights_2 = np.random.uniform(0.0, 1.01, size=(outputNodes, hiddenNodes))
+        self.weights_1 = np.random.uniform(-1.01, 1.01, size=(hiddenNodes, inputNodes))
+        self.weights_2 = np.random.uniform(-1.01, 1.01, size=(outputNodes, hiddenNodes))
         
         # initialize random biases 
-        self.bias_1 = np.random.uniform(0.0, 10.1, size=(hiddenNodes, 1))
-        self.bias_2 = np.random.uniform(0.0, 10.1, size=(outputNodes, 1))
+        self.bias_1 = np.random.uniform(-1.01, 1.01, size=(hiddenNodes, 1))
+        self.bias_2 = np.random.uniform(-1.01, 1.01, size=(outputNodes, 1))
 
     # returns the neural network's guess for the correct output based on the data
     def guess(self, data):
@@ -52,20 +51,20 @@ class NeuralNetwork(object):
 
         # compute error of guess
         error = targets - result
+
+        # backpropagate error to the hidden layer
+        errorh = self.weights_2.transpose().dot(error)
         
         # calculate and add adjustments for the weights
-        gradient2 = self.lr*self.oActivation*(1 - self.oActivation)*error
+        gradient2 = self.lr*2*self.oActivation*(1 - self.oActivation)*error
         delta_weights_2 = gradient2.dot(self.hActivation.transpose())
         self.weights_2 += delta_weights_2
 
         # adjust the bias accordingly
         self.bias_2 += gradient2
 
-        # backpropagate error to the hidden layer
-        errorh = self.weights_2.transpose().dot(error)
-
         # calculate and add adjustments for the weights
-        gradient1 = self.lr*self.hActivation*(1 - self.hActivation)*errorh
+        gradient1 = self.lr*2*self.hActivation*(1 - self.hActivation)*errorh
         delta_weights_1 = gradient1.dot(self.InputData.transpose())
         self.weights_1 += delta_weights_1
 
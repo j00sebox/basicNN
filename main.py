71bf504a22c289cd07b076e3dd4b04903e49dfaa
unknown <josharms00@gmail.com>
2019-12-04 21:54:15 -0500

    Bug Fix: moved the calculation of hidden error before the weight matrix is adjusted.

--- main.py
+++ main.py
@@ -6,17 +6,42 @@ def main():
     # xor example
     xor = NeuralNetwork(2, 3, 2, [0, 1])
 
+    # training data and the respective targets
     inp = [ [[1], [1]], [[0], [1]], [[0], [0]], [[1], [0]]  ]
-    t2 = [ [[1], [0]], [[0], [1]], [[1], [0]], [[0], [1]]  ]
-    t1 = [ [[0]], [[1]] , [[0]], [[1]] ]
- 
-    for i in range(0, 100000):  
-        xor.train(inp[i%4], t2[i%4])
+    targets = [ [[1], [0]], [[0], [1]], [[1], [0]], [[0], [1]]  ]
 
-    print(xor.guess([[1], [1]]))
-    print(xor.guess([[0], [1]]))
-    print(xor.guess([[1], [0]]))
-    print(xor.guess([[0], [0]]))
+    print("Training...")
+    
+    # train for 50000 cycles using stochastic gradient descent
+    for i in range(0, 50000): 
+        # get random data set
+        r = random.randrange(0, 4) 
+        xor.train(inp[r], targets[r])
+    
+    print("Completed training")
+
+    print("Results: ")
+
+    # output network guesses for all possible data
+    t1 = xor.guess([[1], [1]])
+    print('Test 1: ')
+    print('0: ', t1[0])
+    print('1: ', t1[1])
+
+    t2 = xor.guess([[0], [1]])
+    print('Test 2: ')
+    print('0: ', t2[0])
+    print('1: ', t2[1])
+
+    t3 = xor.guess([[1], [0]])
+    print('Test 3: ')
+    print('0: ', t3[0])
+    print('1: ', t3[1])
+
+    t4 = xor.guess([[0], [0]])
+    print('Test 4: ')
+    print('0: ', t4[0])
+    print('1: ', t4[1])
 
 
 if __name__ == "__main__":
--- nn.py
+++ nn.py
@@ -9,10 +9,9 @@ def sigmoid(x):
 
 # Class to instantiate network structure
 class NeuralNetwork(object):
-    hActivation = []
 
     # default learning rate for the network
-    lr = 0.5
+    lr = 0.1
 
     def __init__(self, inputNodes, hiddenNodes, outputNodes, outputLabels):
         # initialize network structure
@@ -24,12 +23,12 @@ class NeuralNetwork(object):
         self.labels = outputLabels
 
         # intialize random weights for both layers
-        self.weights_1 = np.random.uniform(0.0, 1.01, size=(hiddenNodes, inputNodes))
-        self.weights_2 = np.random.uniform(0.0, 1.01, size=(outputNodes, hiddenNodes))
+        self.weights_1 = np.random.uniform(-1.01, 1.01, size=(hiddenNodes, inputNodes))
+        self.weights_2 = np.random.uniform(-1.01, 1.01, size=(outputNodes, hiddenNodes))
         
         # initialize random biases 
-        self.bias_1 = np.random.uniform(0.0, 10.1, size=(hiddenNodes, 1))
-        self.bias_2 = np.random.uniform(0.0, 10.1, size=(outputNodes, 1))
+        self.bias_1 = np.random.uniform(-1.01, 1.01, size=(hiddenNodes, 1))
+        self.bias_2 = np.random.uniform(-1.01, 1.01, size=(outputNodes, 1))
 
     # returns the neural network's guess for the correct output based on the data
     def guess(self, data):
@@ -52,20 +51,20 @@ class NeuralNetwork(object):
 
         # compute error of guess
         error = targets - result
+
+        # backpropagate error to the hidden layer
+        errorh = self.weights_2.transpose().dot(error)
         
         # calculate and add adjustments for the weights
-        gradient2 = self.lr*self.oActivation*(1 - self.oActivation)*error
+        gradient2 = self.lr*2*self.oActivation*(1 - self.oActivation)*error
         delta_weights_2 = gradient2.dot(self.hActivation.transpose())
         self.weights_2 += delta_weights_2
 
         # adjust the bias accordingly
         self.bias_2 += gradient2
 
-        # backpropagate error to the hidden layer
-        errorh = self.weights_2.transpose().dot(error)
-
         # calculate and add adjustments for the weights
-        gradient1 = self.lr*self.hActivation*(1 - self.hActivation)*errorh
+        gradient1 = self.lr*2*self.hActivation*(1 - self.hActivation)*errorh
         delta_weights_1 = gradient1.dot(self.InputData.transpose())
         self.weights_1 += delta_weights_1
 
